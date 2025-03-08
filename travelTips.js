e2530c68dcfd1c01122ace906e57652eeb052c41
FiveOverThree <fiveoverthreetv@gmail.com>
2025-03-08 19:24:17 +0000

    added recommendation images

--- travelTips.js
+++ travelTips.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+
+var results;
+function getRecommendation() {
+    fetch("./travel_tips_api.json")
+        .then(response => response.json())
+        .then(data => results = data)
+        .catch(error => {
+            console.error("Error: ", error);
+        });
+}
